4b7236cebf29ddd036bfd2824748ecf8181f9db7
Ali Omarov <omarovali425@gmail.com>
2023-09-22 13:54:58 +0300

    Second

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -10,7 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
     private PostController controller;
-
+    private static final String POST_METHOD = "POST";
+    private static final String GET_METHOD = "GET";
+    private static final String DELETE_METHOD = "DELETE";
     @Override
     public void init() {
         final var repository = new PostRepository();
@@ -23,19 +25,19 @@ public class MainServlet extends HttpServlet {
         try {
             final var path = req.getRequestURI();
             final var method = req.getMethod();
-            if (method.equals("GET") && path.matches("/api/posts/?$")) {
+            if (method.equals(GET_METHOD) && path.matches("/api/posts/?$")) {
                 controller.all(resp);
                 return;
             }
-            if (method.equals("POST") && path.equals("/api/posts")) {
+            if (method.equals(POST_METHOD) && path.equals("/api/posts")) {
                 controller.save(req.getReader(), resp);
                 return;
             }
-            if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+            if (method.equals(DELETE_METHOD) && path.matches("/api/posts/\\d+")) {
                 controller.removeById(getPostID(path), resp);
                 return;
             }
-            if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+            if (method.equals(GET_METHOD) && path.matches("/api/posts/\\d+")) {
                 controller.getById(getPostID(path), resp);
                 return;
             }
